50db71dafcf38c3f2f1aa4a68c31a7350428ae23
Casey Bowden <casey.bowden@my.jcu.edu.au>
2022-09-12 09:52:16 +1000

    Initial commit

---
+++ Murray's stats day 1.R
@@ -0,0 +1,25 @@
+####Murray's stats course Day 1####
+
+#use terminal for git
+#console for R
+
+#alt - shift - k => keyboard shortcuts
+
+
+#install packages via terminal
+#type R first, click enter, to be in r
+#then install.packages()
+#choose to download from cloud
+
+
+#if package masks a function from another package add its namespace ie stats::filter() -> namespace::function()
+#avoids conflicts
+
+#polymorphism
+
+#git good for keeping track of what has changed in a document
+#good for collaboration because partner gets entire history
+#each change stage is called commit - most recent change is the head - changes are relative to the head - the head can be moved
+#4 stages ; untracked, staged, committed, modified
+  #go to git tab - question mark = untracked
+  #any files or folders you don't want to track -> write in .gitignore
